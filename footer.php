d03da469a965da8d2530a8fb624bcaa1609ca6bb
Mr-Burn <sbu@challoners.com>
2014-02-21 08:18:14 +0000

    Create footer.php
    Links to 'boring' pages like academy information go here.

---
+++ footer.php
@@ -0,0 +1,16 @@
+			<hr class="clear" /><!-- Sorts out any floating elements - stops them from disappearing behind the footer -->
+			<div class="footer"><p>
+				<span class="left lrg">Dr Challoner's Grammar School, &copy; 2005-<? echo date("y"); ?></span>
+				<span class="left sml">DCGS, &copy; 2005-<? echo date("y"); ?></span>
+				<a href="/rebuild/">Home</a> - 
+				<span class="lrg"><a href="/rebuild/about/termdates/">Term dates</a> - </span>
+				<span class="lrg"><a href="/rebuild/about/admissions/">Admissions</a> - </span>
+				<span class="lrg"><a href="/rebuild/about/academyinfo/">Academy information</a> - </span>
+				<span class="lrg"><a href="/rebuild/about/vacancies/">Vacancies</a> - </span>
+				<a href="/rebuild/about/siteinfo/">Privacy and Cookies</a>
+				<span class="lrg"> - <a href="/rebuild/about/contact/">Contact us</a></span>
+			</p></div>
+			
+		</div><!-- This ends page.div -->
+	</body>
+</html>
