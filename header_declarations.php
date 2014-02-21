81996e7ff466a42090e7c45c21ac340cc6430356
Mr-Burn <sbu@challoners.com>
2014-02-21 08:16:50 +0000

    Create header_declarations.php
    meta data, common stylesheets and functions etc, for each page. Note that the <head> tag is closed in header_navigation.php, to allow extra code (principally stylesheets, but also JS etc) to be included in particular pages.

---
+++ header_declarations.php
@@ -0,0 +1,34 @@
+<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; ?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  xmlns:xml="http://www.w3.org/XML/1998/namespace">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Style-Type" content="text/css" />
+		
+		<title>Dr Challoner's Grammar School</title>
+		
+		<link rel="icon" href="/rebuild/main_imgs/favicon.png" />
+		<link rel="shortcut icon" href="/rebuild/main_imgs/favicon.png" />
+		
+		<!-- Homescreen icons for iPhone/iPad. Android should detect these as well. -->
+		<link rel="apple-touch-icon" sizes="57x57" href="/rebuild/main_imgs/apple-icon-60.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="/rebuild/main_imgs/apple-icon-76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="/rebuild/main_imgs/apple-icon-120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="/rebuild/main_imgs/apple-icon-152.png" />
+		
+		<link rel="stylesheet" type="text/css" media="screen" href="/rebuild/styles/general.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (min-device-width : 480px)" href="/rebuild/styles/screen_lrg.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-device-width : 480px)" href="/rebuild/styles/screen_sml.css"/>
+		<?php //Dirty rotten browser hacks
+			if(strpos($_SERVER['HTTP_USER_AGENT'],"Trident") != "") { //Hits IE by spotting the IE rendering engine
+				echo "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"/rebuild/styles/screen_lrg_ie.css\"/>";
+				}
+			if(strpos($_SERVER['HTTP_USER_AGENT'],"iPad") != "") { //Hits iPads by spotting... that they're iPads
+				echo "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"/rebuild/styles/screen_lrg_ipad.css\"/>";
+				echo "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen	and (orientation : portrait)\" href=\"/rebuild/styles/screen_lrg_ipad_portrait.css\"/>";
+				}
+		?>
+		
+		<?php include('php/parsedown.php'); //See parsedown.org ?>
+		
+		<!-- The head tag concludes in header_navigation.php: this allows more code to be added to the head tag. -->
