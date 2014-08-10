20ab0f33fd3f853786eddc86b20734e4515a7d7b
dcgs-sbu <sbu@challoners.com>
2014-08-10 09:05:28 +0000

    Added Google Analytics tracking

--- header_navigation.php
+++ header_navigation.php
@@ -76,6 +76,15 @@
 
 			document.onclick = mclose; // close layer when click-out
 		</script>
+    <script> // Google Analytics
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-53669990-1', 'auto');
+      ga('send', 'pageview');
+    </script>
 
 		<div class="page"><!--On large screens, this constrains the elements to 980px wide and centres them in the browser. On small screens it does nothing. -->
 			<!--googleoff: all--><div class="header"><!--Fixes all these items to the top of the browser-->
