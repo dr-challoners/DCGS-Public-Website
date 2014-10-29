e69f4d8dbf1cee8c07c59e85be152132b3f36115
dcgs-sbu <sbu@challoners.com>
2014-10-29 23:13:53 +0000

    Broke apart main content link on sync menu for quicker updating.

--- sync/index.php
+++ sync/index.php
@@ -28,9 +28,17 @@
 echo "<h1>Content synchronisation</h1>";
 echo "<p>Please select one of the directories below and choose 'update' to synchronise.</p>";
 echo "<p>Choose 'reset' to clear the entire directory and synchronise it from scratch:<br />this is much slower, and should be used only if something has gone wrong.</p>";
-echo "<h2>DCGS public website</h2>";
+echo "<h2>DCGS public website, main content</h2>";
+echo "<p>The main content folder has been separated out to speed up the sync process.<br />If you update more than one of these sections, particularly if you move a file<br />from one section to another, be sure to sync both sections.</p>";
 echo "<ul>"; //Change the initial values as appropriate for the Box folder being referred to
-  echo "<p><li>Main content:      <a href=\"./system_update.php?initial=2176714940\">update</a> or <a href=\"./system_update.php?initial=2176714940&reset=y\">reset</a></li></p>"; //content_main
+  echo "<p><li>Overview:          <a href=\"./system_update.php?initial=2286715709\">update</a> or <a href=\"./system_update.php?initial=2286715709&reset=y\">reset</a></li></p>"; //content_main
+  echo "<p><li>Student Life:      <a href=\"./system_update.php?initial=2286717727\">update</a> or <a href=\"./system_update.php?initial=2286717727&reset=y\">reset</a></li></p>"; //content_main
+  echo "<p><li>Showcase:          <a href=\"./system_update.php?initial=2286720935\">update</a> or <a href=\"./system_update.php?initial=2286720935&reset=y\">reset</a></li></p>"; //content_main
+  echo "<p><li>Information:       <a href=\"./system_update.php?initial=2286712107\">update</a> or <a href=\"./system_update.php?initial=2286712107&reset=y\">reset</a></li></p>"; //content_main
+  echo "<p><li>content_SHARED:    <a href=\"./system_update.php?initial=2296277587\">update</a> or <a href=\"./system_update.php?initial=2296277587&reset=y\">reset</a></li></p>"; //content_main
+echo "</ul>";
+echo "<h2>Other sections on the DCGS website</h2>";
+echo "<ul>";
   echo "<p><li>News stories:      <a href=\"./system_update.php?initial=2209250189\">update</a> or <a href=\"./system_update.php?initial=2209250189&reset=y\">reset</a></li></p>"; //content_news
   echo "<p><li>Intranet links:    <a href=\"./system_update.php?initial=2176731094\">update</a> or <a href=\"./system_update.php?initial=2176731094&reset=y\">reset</a><br /></li></p>"; //content_system/intranet
   echo "<p><li>Override messages: <a href=\"./system_update.php?initial=2176721210\">update</a> or <a href=\"./system_update.php?initial=2176721210&reset=y\">reset</a></li></p>"; //content_system/override
