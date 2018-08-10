b3253eaa1bcef4070c67e572da0234543cc38fdb
sbu <sbu@challoners.com>
2018-08-10 04:29:21 -0400

    Bar links can now be greyed out if no url, and can be put into sets

--- modules/parsing/links.php
+++ modules/parsing/links.php
@@ -1,42 +1,49 @@
 <?php
 
-// Some fallbacks in case users put details in the wrong place
-if (!empty($row['url'])) {
-  $url = $row['url'];
-} else {
-  $url = $row['content'];
-}
-if (!empty($url)) {
-  switch ($dataType) {
-    case 'link':
-      // Gives brand icons to some websites
-      if (strpos($url,"twitter.com") !== false) {
+switch ($dataType) {
+  case 'link':
+    // Gives brand icons to some websites
+    if (!empty($row['url'])) {
+      if (strpos($row['url'],"twitter.com") !== false) {
         $linkIcon = 'twitter';
-      } elseif (strpos($url,"instagram.com") !== false) {
+      } elseif (strpos($row['url'],"instagram.com") !== false) {
         $linkIcon = 'instagram';
       } else {
         $linkIcon = 'link';
       }
-      break;
-    case 'file':
-      $linkIcon = 'file';
-      break;
-    case 'email':
-      $linkIcon = 'envelope-o';
-      // Add the 'mailto:' component and some simple robot baffling
-      $address = ""; $i = 0;
-      for ($i = 0; $i < strlen($url); $i++) { $address .= '&#'.ord($url[$i]).';'; }
-      $url = "mailto:".$address; 
-      break;
-  }
-  $content = '<a target="'.mt_rand().'" class="barLink btn btn-default btn-block" href="'.$url.'" role="button">';
-  $content .= '<i class="fa fa-'.$linkIcon.'"></i>';
-  if (!empty($row['content'])) {
-    $content .= formatText($row['content'],0);
-  } else {
-    $content .= $row['url'];
-  }
-  $content .= '</a>';
+    } else {
+      $linkIcon = 'link';
+    }
+    break;
+  case 'file':
+    $linkIcon = 'file';
+    break;
+  case 'email':
+    $linkIcon = 'envelope-o';
+    // Add the 'mailto:' component and some simple robot baffling
+    $address = ""; $i = 0;
+    if (!empty($row['url'])) {
+      for ($i = 0; $i < strlen($row['url']); $i++) { $address .= '&#'.ord($row['url'][$i]).';'; }
+      $row['url'] = "mailto:".$address; 
+    }
+    break;
+}
+
+if (!empty($row['url'])) {
+  $content = '<a target="'.mt_rand().'" class="barLink btn btn-default btn-block" href="'.$row['url'].'" role="button">';
+} else {
+  $content = '<a class="barLink-disabled btn btn-default btn-block" disabled role="button">';
+}
+$content .= '<i class="fa fa-'.$linkIcon.'"></i>';
+if (!empty($row['content'])) {
+  $content .= formatText($row['content'],0);
+} else {
+  $content .= $row['url'];
+}
+$content .= '</a>';
+if (isset($set)) {
+  $output['content'][$set]['set'][] = $content; 
+} else {
   $output['content'][] = $content;
 }
 
