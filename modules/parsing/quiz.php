e62f9334f9c6a663040e1767761e069ac334e6ea
Cameron Robey <cameron@cameronrobey.co.uk>
2018-08-15 21:18:16 +0100

    fix parsing errors

--- modules/parsing/quiz.php
+++ modules/parsing/quiz.php
@@ -1,20 +1,22 @@
 <?php
 
 // Javascript modules that only need to be loaded if a quiz is present
-$quiz = '<script type='text/javascript' src="/modules/quiz/quiz.js"></script>
-        <script type='text/javascript' src="/modules/js/md5.js"></script>
-        <script type='text/javascript' src="/modules/js/jquery.base64.js"></script>
-        <script src="/modules/js/jquery.pietimer.min.js"></script><script>';
+if (file_exists($_SERVER["DOCUMENT_ROOT"].'/data/quiz/'.$row['content'].'.json')) {
+    $quiz = '<script type="text/javascript" src="/modules/quiz/quiz.js"></script>
+            <script type="text/javascript" src="/modules/js/md5.js"></script>
+            <script type="text/javascript" src="/modules/js/jquery.base64.js"></script>
+            <script src="/modules/js/jquery.pietimer.min.js"></script>';
 
-// Div quiz is constructed in
-$quiz .= '<div id="root"></div>';
+    // Div quiz is constructed in
+    $quiz .= '<div id="root"></div>';
 
-// Script to initialise quiz
-$quiz .= '<script>
-        let q = new Quiz("root");
-        q.generateQuiz('.$row['content'].');
-    </script>';
+    // Script to initialise quiz
+    $quiz .= '<script>
+            let q = new Quiz("root");
+            q.generateQuiz("'.$row['content'].'");
+        </script>';
 
-$output['content'][] = $quiz;
+    $output['content'][] = $quiz;
+}
 
 ?>
