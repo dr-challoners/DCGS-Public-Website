297143d0ecdcf459d2314baf7fd6354805e4cb70
sbu <sbu@challoners.com>
2022-09-18 11:29:47 -0400

    Link to Sports website on front page

--- index.php
+++ index.php
@@ -57,7 +57,10 @@
     <a href="https://files.ofsted.gov.uk/v1/file/50147384" target="<?php echo 'page'.mt_rand(); ?>"><img class="img-responsive" id="ofstedLogo" src="/img/ofsted-outstanding.jpg" alt="Ofsted Outstanding Provider" /></a>
     <a href="https://www.astrahub.org/" target="<?php echo 'page'.mt_rand(); ?>"><img class="img-responsive" id="astraLogo" src="/img/astraTSHlogo.jpg" alt="Astra Teaching School Hub Buckinghamshire" /></a>
     <div class="twitterColumn" id="sports_team">
-      <a href="https://twitter.com/DCGSSport" target="<?php echo 'page'.mt_rand(); ?>">DCGS Sport <span class="pull-right"><i class="fab fa-twitter"></i></span></a>
+      <a href="https://sport.challoners.com/">
+        <h3>DCGS Sport</h3>
+        <p>sport.challoners.com</p>
+      </a>
 		  <a class="twitter-timeline" data-height="1560" data-chrome="noheader nofooter" data-link-color="#9B0B0B" href="https://twitter.com/DCGSSport?ref_src=twsrc%5Etfw">Tweets by DCGSSport</a>
     </div>
   </div>
